b4ba01d50d3884ae7de1101e79de08e1c5b904fc
myrnamodolonlima <mmodolonlima@gmail.com>
2024-02-17 16:16:13 -0500

    Fixing survey missing file.

--- settings.py
+++ settings.py
@@ -2,12 +2,10 @@ from os import environ
 
 
 SESSION_CONFIGS = [
-    dict(
-        name='guess_two_thirds',
-        display_name="Guess 2/3 of the Average",
-        app_sequence=['guess_two_thirds', 'payment_info'],
-        num_demo_participants=3,
-    ),
+    #dict(
+      #  name='guess_two_thirds',
+      #  display_name="Guess 2/3 of the Average",
+       #
     dict(
         name="pubgoods", app_sequence=['pubgoods', 'payment_info'], num_demo_participants=2,
     )
